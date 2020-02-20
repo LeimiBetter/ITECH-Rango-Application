af91ebfdd8c4af5e70612b28931218fe7ee66802
雷米 <leimi@leimideMacBook-Air.local>
2020-02-20 14:17:52 +0000

    chapter10_exercise

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
+# Create your models here.
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+
+
 class Category(models.Model):
+    NAME_MAX_LENGTH = 200
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -11,7 +22,10 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Page(models.Model):
+    TITLE_MAX_LENGTH = 200
+    URL_MAX_LENGTH = 200
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
@@ -19,3 +33,12 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
